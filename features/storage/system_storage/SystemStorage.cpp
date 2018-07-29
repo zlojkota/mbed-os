1e96c278a35832b28e802e1147d895f035592e6e
Yossi Levy <Yossi.Levy@arm.com>
2018-07-29 18:55:40 +0300

    Moving SD, SPIF and FLASHIAP into mbedos and refactoring features storage directory structure.

---
+++ features/storage/system_storage/SystemStorage.cpp
@@ -0,0 +1,53 @@
+#include "SystemStorage.h"
+
+namespace mbed {
+namespace SystemStorage {
+
+MBED_WEAK BlockDevice *get_default_blockdevice()
+{
+#if COMPONENT_SPIF
+
+    static SPIFBlockDevice default_bd(MBED_CONF_SPIF_DRIVER_SPI_MOSI, MBED_CONF_SPIF_DRIVER_SPI_MISO,
+                                      MBED_CONF_SPIF_DRIVER_SPI_CLK, MBED_CONF_SPIF_DRIVER_SPI_CS, MBED_CONF_SPIF_DRIVER_SPI_FREQ);
+
+    return &default_bd;
+
+#elif COMPONENT_SD
+
+    static SDBlockDevice default_bd(MBED_CONF_SD_SPI_MOSI, MBED_CONF_SD_SPI_MISO,
+                                    MBED_CONF_SD_SPI_CLK, MBED_CONF_SD_SPI_CS);;
+
+    return &default_bd;
+
+#else
+
+    return NULL;
+
+#endif
+
+}
+
+MBED_WEAK FileSystem *get_default_filesystem()
+{
+#if COMPONENT_SPIF
+
+    static LittleFileSystem default_fs("defult_fs", get_default_blockdevice());
+
+    return &default_fs;
+
+#elif COMPONENT_SD
+
+    static FATFileSystem default_fs("defult_fs", get_default_blockdevice());
+
+    return &default_fs;
+
+#else
+
+    return NULL;
+
+#endif
+
+}
+
+}
+}
