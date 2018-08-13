780a71236a7e495196926ab3061e094145ab9561
Amir Cohen <the.amiro.cohen@gmail.com>
2018-08-13 14:57:25 +0300

    Add FileSystem Tests

--- features/storage/system_storage/SystemStorage.cpp
+++ features/storage/system_storage/SystemStorage.cpp
@@ -1,21 +1,30 @@
 #include "SystemStorage.h"
 
 namespace mbed {
-namespace SystemStorage {
+namespace SystemStorage{
 
-MBED_WEAK BlockDevice *get_default_blockdevice()
+MBED_WEAK BlockDevice* get_default_blockdevice()
 {
 #if COMPONENT_SPIF
 
-    static SPIFBlockDevice default_bd(MBED_CONF_SPIF_DRIVER_SPI_MOSI, MBED_CONF_SPIF_DRIVER_SPI_MISO,
-                                      MBED_CONF_SPIF_DRIVER_SPI_CLK, MBED_CONF_SPIF_DRIVER_SPI_CS, MBED_CONF_SPIF_DRIVER_SPI_FREQ);
+    static SPIFBlockDevice default_bd(
+        MBED_CONF_SPIF_DRIVER_SPI_MOSI, 
+        MBED_CONF_SPIF_DRIVER_SPI_MISO,
+        MBED_CONF_SPIF_DRIVER_SPI_CLK, 
+        MBED_CONF_SPIF_DRIVER_SPI_CS, 
+        MBED_CONF_SPIF_DRIVER_SPI_FREQ
+        );
 
     return &default_bd;
 
 #elif COMPONENT_SD
 
-    static SDBlockDevice default_bd(MBED_CONF_SD_SPI_MOSI, MBED_CONF_SD_SPI_MISO,
-                                    MBED_CONF_SD_SPI_CLK, MBED_CONF_SD_SPI_CS);;
+    static SDBlockDevice default_bd(
+        MBED_CONF_SD_SPI_MOSI, 
+        MBED_CONF_SD_SPI_MISO,
+        MBED_CONF_SD_SPI_CLK, 
+        MBED_CONF_SD_SPI_CS
+        );
 
     return &default_bd;
 
@@ -27,7 +36,7 @@ MBED_WEAK BlockDevice *get_default_blockdevice()
 
 }
 
-MBED_WEAK FileSystem *get_default_filesystem()
+MBED_WEAK FileSystem * get_default_filesystem()
 {
 #if COMPONENT_SPIF
 
